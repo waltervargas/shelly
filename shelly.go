712ab39ee7b84229a5f8c3ee04d976a7f53a1392
Walter Vargas <waltervargas@linux.com>
2022-10-28 04:13:34 +0200

    Add session to shelly

--- shelly.go
+++ shelly.go
@@ -2,6 +2,7 @@ package shelly
 
 import (
 	"fmt"
+	"io"
 	"os/exec"
 	"strings"
 )
@@ -12,4 +13,19 @@ func CmdFromString(cmd string) (*exec.Cmd, error) {
 		return nil, fmt.Errorf("unable to create a command from empty string: %s", cmd)
 	}
 	return exec.Command(args[0], args[1:]...), nil
+}
+
+type Session struct {
+	Stdin          io.Reader
+	Stdout, Stderr io.Writer
+	DryRun         bool
+}
+
+func NewSession(stdin io.Reader, stdout, stderr io.Writer) *Session {
+	return &Session{
+		Stdin:  stdin,
+		Stdout: stdout,
+		Stderr: stderr,
+		DryRun: false,
+	}
 }
--- shelly_test.go
+++ shelly_test.go
@@ -1,6 +1,10 @@
 package shelly_test
 
 import (
+	"bytes"
+	"io"
+	"os"
+	"strings"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
@@ -40,5 +44,35 @@ func TestCmdFromStringErrorsOnEmptyInput(t *testing.T) {
 	_, err := shelly.CmdFromString("")
 	if err == nil {
 		t.Fatal("want error on empty input, got nil")
+	}
+}
+
+func TestNewSession(t *testing.T) {
+	t.Parallel()
+	stdin := os.Stdin
+	stdout := os.Stdout
+	stderr := os.Stderr
+	want := shelly.Session{
+		Stdin:  stdin,
+		Stdout: stdout,
+		Stderr: stderr,
+	}
+	got := *shelly.NewSession(stdin, stdout, stderr)
+	if want != got {
+		t.Errorf("want %#v, got %#v", want, got)
+	}
+}
+
+func TestRun(t *testing.T) {
+	t.Parallel()
+	stdin := strings.NewReader("echo hello\n\n")
+	stdout := &bytes.Buffer{}
+	session := shelly.NewSession(stdin, stdout, io.Discard)
+	session.DryRun = true
+	session.Run()
+	want := "> hello\n> > \nBe seeing you!\n"
+	got := stdout.String()
+	if !cmp.Equal(want, got) {
+		t.Error(cmp.Diff(want, got))
 	}
 }
